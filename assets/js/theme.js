d9a336eaf395dd363e5457d4e70ae7ab8f0ab240
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2023-02-12 20:33:10 -0300

    Adicionado novos arquivos

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,6 +14,24 @@ $(document).ready(function(){
       $(this).addClass("active");
     });
 });
+//Carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = $(".slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides.eq(i).hide();  
+  }
+  slides.eq(slideIndex-1).show();  
+}
 
   
   
